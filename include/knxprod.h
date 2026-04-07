011461f81aa3740c6670fa516b8c292a93b502c2
Michael Geramb <lasso_riefen0m@icloud.com>
2026-04-07 06:55:28 +0200

    Beta 0.2.1 Fix not forwarded room temp after window open

--- include/knxprod.h
+++ include/knxprod.h
@@ -18,7 +18,7 @@
 #define MAIN_OpenKnxId 0xAE
 #define MAIN_ApplicationNumber 58
 #define MAIN_ApplicationVersion 2
-#define MAIN_FirmwareRevision 0
+#define MAIN_FirmwareRevision 1
 #define MAIN_ApplicationEncoding iso-8859-15
 #define MAIN_ParameterSize 9827
 #define MAIN_MaxKoNumber 1089
